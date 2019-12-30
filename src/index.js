60b0c8136766edfeecd891cb403fe4d882ff4374
dequan300 <48841256+dequan300@users.noreply.github.com>
2019-12-29 19:21:47 -0500

    update
    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-import List from "../public/list";
 import App from "./App";
 
 ReactDom.render(
   <div>
     <App />
-    <p>created by </p>
+    <p>created by dequan</p>
     <p>copyright @ {new Date().getFullYear()}</p>
   </div>,
   document.getElementById("root")
